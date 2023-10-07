8dff6ece7047d3f23cd56cdca0ba679062496c2a
Ashish <thankiashish555@gmail.com>
2023-10-07 13:13:06 +0530

    Login & Register State Management

--- pmsp_client/src/screens/Login.jsx
+++ pmsp_client/src/screens/Login.jsx
@@ -1,22 +1,42 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Login = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(formData);
+  };
   return (
     <div className="container">
       <div className="login-container">
-        <form className="login-form">
+        <form className="login-form" onSubmit={handleSubmit}>
           <h1>Login</h1>
           <input
             type="email"
             name="email"
             id="email"
             placeholder="Email Address"
+            value={formData.email}
+            onChange={handleInputChange}
           />
           <input
             type="password"
             name="password"
             id="password"
             placeholder="Password"
+            value={formData.password}
+            onChange={handleInputChange}
           />
           <input type="submit" value="LOGIN" />
           <p className="bottom-nav">
