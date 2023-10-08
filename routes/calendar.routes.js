f77c40dc0ebf4519055377640e973a76772a1228
namanthanki <stuniz7855@gmail.com>
2023-10-08 08:56:30 +0530

    added scopes

--- routes/calendar.routes.js
+++ routes/calendar.routes.js
@@ -3,5 +3,5 @@ const CalendarController = require('../controllers/calendar.controller');
 
 router.get("/api/google", CalendarController.googleInit);
 router.get("/api/google/redirect", CalendarController.googleRedirect);
-router.get("/api/google/schedule-event", CalendarController.scheduleEvent);
+router.post("/api/google/schedule-event", CalendarController.scheduleEvent);
 module.exports = router;
